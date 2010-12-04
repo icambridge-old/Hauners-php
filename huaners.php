8271aa8184cc8b8595f2a3a20c31a8d64bc5f392
Iain Cambridge <wackiebackie@gmail.com>
2010-12-04 13:20:22 +0000

    Added option validation and wp config function

--- huaners.php
+++ huaners.php
@@ -1,5 +1,9 @@
 #!/usr/bin/php
 <?php
+
+	date_default_timezone_set('America/Los_Angeles');
+	error_reporting(-1);
+	ini_set("display_errors",1);
 	/**
 	 * Script to back up MySQL and directories to various
 	 * places using various methods such as FTP, SCP, 
@@ -13,95 +17,187 @@
 	 */
 	 
 	define('ROOT_DIR', dirname(__FILE__));
+	// SQL
+	define('SQL_USERNAME', '');
+	define('SQL_PASSWORD', '');
+	define('SQL_HOSTNAME', '');
+	define('SQL_DATABASE', '');
+	define('SQL_BACKUP', true);
+	// AWS
+	define('AWS_ACCESS', '');
+	define('AWS_SECRET', '');
+	define('AWS_BUCKET', '');
+	define('AWS_BACKUP', true);
+	// FTP
+	define('FTP_USERNAME', '');
+	define('FTP_PASSWORD', '');
+	define('FTP_HOSTNAME', '');
+	define('FTP_PORT', '');
+	define('FTP_BACKUP', '');
+	// SMTP
+	define('SMTP_USERNAME', '');
+	define('SMTP_PASSWORD', '');
+	define('SMTP_HOSTNAME', '');
+	define('SMTP_PORT', '');
+	define('EMAIL_REPORT', true);
+	define('ATTACH_BACKUP', true);
+	
+	$shortOpts = "wtqd:";
+	$longOpts = array("help","wordpress","dir:");
+	
+	$options  = getopt($shortOpts, $longOpts);
+	
+	if(array_key_exists('help', $options) || $GLOBALS['argc'] == 1){
+		print "Huaners version 1.0".PHP_EOL;
+		print "===================".PHP_EOL;
+		print " -w (--wordpress) - Does wordpress backup.".PHP_EOL;
+		print " -d (--dir) <dir> - The directory that is to be backed up.".PHP_EOL;
+		print " -t (--transfer)  - Does the transfer of files according to settings in file.".PHP_EOL;
+		print " -q (--quiet)     - Doesn't print out any messages.".PHP_EOL;
+		exit;
+	}
+	
+	if ( !array_key_exists("dir",$options) && !array_key_exists("d",$options) ){
+		print "Directory must be defined".PHP_EOL;
+	} else {
+		$dir = ( array_key_exists("dir", $options) ) ? $options["dir"] : $options["d"];
+		define("BACKUP_DIR",$dir);
+	}
+	
+	if ( (array_key_exists("transfer", $options) || array_key_exists("t",$options) ) ){
+		
+		define("TRANSFER",true);
+		// Check dependicies
+		if ( AWS_BACKUP === true ){
+			if ( !is_readable(ROOT_DIR."/lib/awssdk/sdk.class.php") ){
+				print "AWS PHP SDK not found, please download and put in ".ROOT_DIR."/lib/awssdk/sdk.class.php".PHP_EOL;
+				exit;
+			} 			
+			require_once ROOT_DIR."/lib/awssdk/sdk.class.php";
+		}
+		
+		if ( FTP_BACKUP === true ){
+			if ( !extenstion_loaded("ftp") ) {
+				print "PHP Doesn't have ftp enabled".PHP_EOL;
+				exit;
+			}
+		}
+		
+	} else {
+		define("TRANSFER",false);
+	}
+	
+	if ( array_key_exists("wordpress", $options) || array_key_exists("w",$options) ) {		
+		define("WORDPRESS",true);			
+	} else {
+		define("WORDPRESS",false);
+	}
+	
+	if ( array_key_exists("quiet", $options) || array_key_exists("q",$options) ){
+		define("QUIET", true);
+	} else {
+		define("QUIET", false);
+	}
+	// Move this to function if/when I add other cmses
+	$backupMode = ( WORDPRESS === true ) ? "WordPress" : "Normal"; 
+	
+	qprint("Huaners 1.0");
+	qprint("================");
+	qprint("Archiving : ".BACKUP_DIR);
+	qprint("Mode : ".$backupMode);
+	qprint("Starting...");	
+	
+	
+	/**
+	 * Handles the printing of non error messages,
+	 * got the idea from Tyler Hall's autosmush.
+	 * 
+	 * @since 1.0
+	 */
+	function qprint($string){
+		if (!QUIET){
+			print $string.PHP_EOL;
+		}	
+	}
+	
+	/**
+	 * Handles the archiving of the directory using
+	 * tar.
+     * 
+	 * @return string the location of the archive. 
+	 */
+	function archiveDir(){
+		
+		$archiveFile = date("m-d-y")."-backup.tar.gz";
+		
+		exec("tar -C ".ROOT_DIR." -zcf ".." ".
+			,$output,$status);
+			
+		if ( !file_exists(ROOT_DIR."./".$archiveFile) ){
+			qprint("Unable to create archive file",true);
+		}
 
-	$shortOps = 'c';
+		return ROOT_DIR."/".$archiveFile;
+		
+	}
 	
-	$longOpts = array();
-	// Options for AWS
-	$awsOpts = array("accesskey:","secretkey:","bucket:");
-	// Options for SQL
-	$sqlOpts = array("sqluser:","sqlpass:","sqlhost:","sqldb:","sqltype:","sqlport:");
-	// Options for SCP
-	$scpOpts = array("scpuser:","scphost:","scpidfile:","scpport:");
-	// Options for FTP
-	$ftpOpts = array("ftpuser:","ftppass:","ftphost:","ftpport:");
-	// Options for Email
-	$emailOpts = array("emailto:","smtpuser:","smtppass:","smtphost:","smtpport:");
-	// General Options
-	$generalOpts = array("help","wordpress");
-	// Put into a single array.
-	$longOpts = array_merge($longOpts,$awsOpts,$sqlOpts,$scpOpts,$ftpOpts,$generalOpts);
+	/**
+	 * Upload the backup file to 
+	 * @param unknown_type $archiveFile
+	 */
+	function uploadAws($archiveFile){
+
+		$s3 = new AmazonS3(AWS_ACCESS,AWS_SECRET);
+		if ( !$s3->if_bucket_exists(AWS_BUCKET) ){
+			qprint("Amazon S3 bucket doesn't exist",true);
+			exit;
+		}		
+				
+	}
 	
+	/**
+	 * 
+     *
+	 */
+	function getWpConfig(){
+		
+		if ( !is_readable(BACKUP_DIR."/wp-config") ){
+			qprint("WordPress config file not found",true);
+		}
+		
+		$wpConfig = file_get_contents(BACKUP_DIR."/wp-config.php");
+		
+		preg_match("~define(\'DB_NAME\', \'(.*)\');~isU",$wpConfig,$wpDatabase);
+		preg_match("~define(\'DB_USER\', \'(.*)\');~isU",$wpConfig,$wpUsername);
+		preg_match("~define(\'DB_PASS\', \'(.*)\');~isU",$wpConfig,$wpPassword);
+		preg_match("~define(\'DB_HOST\', \'(.*)\');~isU",$wpConfig,$wpHostname);
 	
-	$options  = getopt($shortOps, $longOpts);
-	
-	if(array_key_exists('help', $options) || $GLOBALS['argc'] == 1)
-    {
-        echo "Usage: " . $GLOBALS['argv'][0] . PHP_EOL;
-        echo "-c : Runs using the settings in the configuration file. Recommended for crontasks.".PHP_EOL;
-        echo PHP_EOL;
-        echo "aws options".PHP_EOL;
-        echo "-----------".PHP_EOL;
-        echo "--accesskey <access key> : The access key for AWS".PHP_EOL;
-        echo "--secretkey <secret key> : The secret key for AWS".PHP_EOL;
-        echo "--bucket <bucket name>   : The name of the bucket that will be used".PHP_EOL;
-        echo PHP_EOL;
-        echo "sql options".PHP_EOL;
-        echo "-----------".PHP_EOL;
-        echo "--sqluser <username> : Username for SQL access".PHP_EOL;
-        echo "--sqlpass <password> : Password for SQL access".PHP_EOL;
-        echo "--sqlhost <hostname> : Hostname/IP that the SQL server is using".PHP_EOL;
-        echo "--sqlport <port>     : The port number that the SQL server is bound to.".PHP_EOL;
-        echo "--sqldb <dbname>     : The name of the database to backup.".PHP_EOL; 
-        echo PHP_EOL;
-        echo "FTP options".PHP_EOL;
-        echo "--ftpuser <username> : Username for FTP access.".PHP_EOL;
-        echo "--ftppass <password> : Password for FTP access.".PHP_EOL;
-        echo "--ftphost <hostname> : Hostname/IP that the FTP is on.".PHP_EOL;
-        echo "--ftpport <port>     : Port number that the FTP server is bound to.".PHP_EOL;
-        echo PHP_EOL;
-        echo "SCP options".PHP_EOL;
-        echo "--scpuser <username> : Username for SCP access.".PHP_EOL;
-        echo "--scphost <hostname> : Hostname/IP for SCP access.".PHP_EOL;
-        echo "--scpidfile <file>   : Location of the file to be used as an identy file by scp.".PHP_EOL;
-        echo "--scppport <port>    : The port number SCP is bound to".PHP_EOL;
-        echo PHP_EOL;
-        echo "Email Options".PHP_EOL;
-        echo "--emailto <email>     : The email address the backups/reports are to be sent".PHP_EOL;
-        echo "--smtpuser <username> : The username for SMTP auth".PHP_EOL;
-        echo "--smtppass <password> : The password for SMTP auth".PHP_EOL;
-        echo "--smtphost <hostname> : The hostname/IP for the SMTP server".PHP_EOL;
-        echo "--smtpport <port>     : The port number the SMTP server is bound too".PHP_EOL;
-        
-        exit;
-    }
-    
-    if ( !is_readable(ROOT_DIR."/etc/config.php") ){
-    	echo "Fatal Error".PHP_EOL;
-    	echo "Unable to read configuration file".PHP_EOL;
-    	echo "Please check your permissions on ".ROOT_DIR."/etc/config.php".PHP_EOL;
-    	exit;
-    }
-	
-    require_once ROOT_DIR.'/etc/config.php';
-    require_once ROOT_DIR.'/lib/Huaner/Core.php';
-    
-    if ( $generalSettings['sample'] === true ){
-    	echo "Fatal Error".PHP_EOL;
-    	echo "Configuration is sample configuration.".PHP_EOL;
-    	echo "Please edit the configuration file".PHP_EOL;
-    	exit;
-    } 
-    
-    // prepare configuration.
-    // TODO neaten up
-    $config = array();
-    $config['general'] = $generalSettings;
-    $config['aws'] = $awsSettings;
-    $config['ftp'] = $ftpSettings;
-    $config['email'] = $mailSettings;
-    $config['sql'] = $sqlSettings;
-    $config['scp'] = $scpSettings;
-    
-    HuanersCore::setConfig($config);
-  //  HuanersCore::handleOptions($options);
+		if ( is_array($wpDatabase) && array_key_exists(1, $wpDatabase ) ){
+			define("WP_DB_NAME",$wpDatabase[1]);
+		} else {
+			qprint("Unable to find database name from config",true);
+			exit;
+		}
+		
+		if ( is_array($wpUsername) && array_key_exists(1, $wpUsername) ){
+			define("WP_DB_NAME",$wpUsername[1]);
+		} else {
+			qprint("Unable to find database user from config",true);
+			exit;
+		}	
+		
+		if ( is_array($wpPassword) && array_key_exists(1, $wpPassword) ){
+			define("WP_DB_PASS",$wpPassword[1]);
+		} else {
+			qprint("Unable to find database password from config",true);
+			exit;
+		}
+		
+		if ( is_array($wpHostname) && array_key_exists(1, $wpHostname) ){
+			define("WP_DB_HOST",$wpHostname[1]);
+		} else {
+			qprint("Unable to find database hostname from config",true);
+			exit;
+		}
+		
+	}
