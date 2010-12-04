85bff68c39f8cc34f79c84ade730cc9ba53ab025
Iain Cambridge <wackiebackie@gmail.com>
2010-12-04 16:25:31 +0000

    WordPress & S3 uploading working

--- huaners.php
+++ huaners.php
@@ -1,9 +1,5 @@
 #!/usr/bin/php
 <?php
-
-	date_default_timezone_set('America/Los_Angeles');
-	error_reporting(-1);
-	ini_set("display_errors",1);
 	/**
 	 * Script to back up MySQL and directories to various
 	 * places using various methods such as FTP, SCP, 
@@ -33,7 +29,7 @@
 	define('FTP_PASSWORD', '');
 	define('FTP_HOSTNAME', '');
 	define('FTP_PORT', '');
-	define('FTP_BACKUP', '');
+	define('FTP_BACKUP', false);
 	// SMTP
 	define('SMTP_USERNAME', '');
 	define('SMTP_PASSWORD', '');
@@ -41,6 +37,11 @@
 	define('SMTP_PORT', '');
 	define('EMAIL_REPORT', true);
 	define('ATTACH_BACKUP', true);
+	
+	// Set stuff that need set :~)
+	date_default_timezone_set('America/Los_Angeles');
+	error_reporting(-1);
+	ini_set("display_errors",1);
 	
 	$shortOpts = "wtqd:";
 	$longOpts = array("help","wordpress","dir:");
@@ -61,7 +62,7 @@
 		print "Directory must be defined".PHP_EOL;
 	} else {
 		$dir = ( array_key_exists("dir", $options) ) ? $options["dir"] : $options["d"];
-		define("BACKUP_DIR",$dir);
+		define("BACKUP_DIR",ROOT_DIR."/".$dir);
 	}
 	
 	if ( (array_key_exists("transfer", $options) || array_key_exists("t",$options) ) ){
@@ -101,12 +102,33 @@
 	// Move this to function if/when I add other cmses
 	$backupMode = ( WORDPRESS === true ) ? "WordPress" : "Normal"; 
 	
+	define("TMP_DIR", ROOT_DIR."/huaners-tmp");
+	if ( file_exists(TMP_DIR) ){
+		exec("rm -Rf ".TMP_DIR);
+	}
+	mkdir(TMP_DIR);
+	
 	qprint("Huaners 1.0");
 	qprint("================");
 	qprint("Archiving : ".BACKUP_DIR);
 	qprint("Mode : ".$backupMode);
-	qprint("Starting...");	
+	qprint("Starting...");		
 	
+	exec('cp -R '.BACKUP_DIR.' '.TMP_DIR,$output,$result);
+	
+	if ( WORDPRESS ){
+		getWpConfig();
+	}
+	getDb();
+	$archiveFile = archiveDir();
+	
+	if ( TRANSFER ){
+		uploadAws($archiveFile);
+	}
+	
+	qprint("Finished");
+	
+	exec("rm -Rf ".TMP_DIR);
 	
 	/**
 	 * Handles the printing of non error messages,
@@ -128,15 +150,19 @@
 	 */
 	function archiveDir(){
 		
+		qprint("Starting archive creation");
 		$archiveFile = date("m-d-y")."-backup.tar.gz";
 		
-		exec("tar -C ".ROOT_DIR." -zcf ".." ".
+		exec("tar -C ".ROOT_DIR." -zcf ".$archiveFile." ".TMP_DIR
 			,$output,$status);
 			
-		if ( !file_exists(ROOT_DIR."./".$archiveFile) ){
+		if ( !file_exists(ROOT_DIR."/".$archiveFile) ){			
 			qprint("Unable to create archive file",true);
+			exit;
 		}
 
+		qprint("Archive created");
+		
 		return ROOT_DIR."/".$archiveFile;
 		
 	}
@@ -147,40 +173,57 @@
 	 */
 	function uploadAws($archiveFile){
 
+		if ( !AWS_BACKUP ){
+			return;
+		}
+		
+		qprint("Starting S3 Upload");
+		
 		$s3 = new AmazonS3(AWS_ACCESS,AWS_SECRET);
 		if ( !$s3->if_bucket_exists(AWS_BUCKET) ){
 			qprint("Amazon S3 bucket doesn't exist",true);
 			exit;
-		}		
-				
+		}
+		
+		$options = array(
+                     "fileUpload" => $archiveFile,
+                     "acl" => AmazonS3::ACL_PRIVATE, // Don't want people walking away with the dump
+                     "contentType" => "application/x-gzip",
+                   );
+		$s3->create_object(AWS_BUCKET, basename($archiveFile), $options);		
+		
+		qprint("Archive Uploaded to S3");
 	}
 	
 	/**
+	 * Fetches the SQL details for WordPress
+	 * from wp-config.php.
 	 * 
-     *
+     * @since 1.0
 	 */
 	function getWpConfig(){
 		
-		if ( !is_readable(BACKUP_DIR."/wp-config") ){
+		if ( !is_readable(BACKUP_DIR."/wp-config.php") ){
 			qprint("WordPress config file not found",true);
 		}
 		
 		$wpConfig = file_get_contents(BACKUP_DIR."/wp-config.php");
 		
-		preg_match("~define(\'DB_NAME\', \'(.*)\');~isU",$wpConfig,$wpDatabase);
-		preg_match("~define(\'DB_USER\', \'(.*)\');~isU",$wpConfig,$wpUsername);
-		preg_match("~define(\'DB_PASS\', \'(.*)\');~isU",$wpConfig,$wpPassword);
-		preg_match("~define(\'DB_HOST\', \'(.*)\');~isU",$wpConfig,$wpHostname);
-	
+		preg_match("~define\('DB_NAME', '(.*)'\);~isU",$wpConfig,$wpDatabase);
+		preg_match("~define\('DB_USER', '(.*)'\);~isU",$wpConfig,$wpUsername);
+		preg_match("~define\('DB_PASSWORD', '(.*)'\);~isU",$wpConfig,$wpPassword);
+		preg_match("~define\('DB_HOST', '(.*)'\);~isU",$wpConfig,$wpHostname);
+		
 		if ( is_array($wpDatabase) && array_key_exists(1, $wpDatabase ) ){
 			define("WP_DB_NAME",$wpDatabase[1]);
 		} else {
+			var_dump($wpDatabase);
 			qprint("Unable to find database name from config",true);
 			exit;
 		}
 		
 		if ( is_array($wpUsername) && array_key_exists(1, $wpUsername) ){
-			define("WP_DB_NAME",$wpUsername[1]);
+			define("WP_DB_USER",$wpUsername[1]);
 		} else {
 			qprint("Unable to find database user from config",true);
 			exit;
@@ -200,4 +243,26 @@
 			exit;
 		}
 		
+	}
+	
+	/**
+	 * Gets the database dump
+	 * 
+	 * @since 1.0
+	 */
+	function getDb(){
+		
+		if ( !SQL_BACKUP && !WORDPRESS ){
+			return false;
+		}
+		
+		if ( WORDPRESS ){
+			exec("mysqldump --host='".WP_DB_HOST."' --user='".WP_DB_USER."' --password='".WP_DB_PASS."' --database ".WP_DB_NAME." >> ".TMP_DIR."/".WP_DB_NAME."-backup.sql");			
+		} else {
+			exec("mysqldump --host='".SQL_HOSTNAME."' --user='".SQL_USERNAME."' --password='".SQL_PASSWORD."' --database ".SQL_DATABASE." >> ".TMP_DIR."/".SQL_DATABASE."-backup.sql");	
+		}
+	
+		qprint("Dumped database");
+		
+		return true;
 	}
